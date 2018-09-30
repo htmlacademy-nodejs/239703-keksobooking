320c9a96f5fabf23c0d37994efcab6b1571665b4
okoltun <okoltun@shift4.com>
2018-09-30 18:03:17 +0300

    add cli interface

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-const validCommands = [
-    '--version',
-    '--help'
+const validCoomands = [
+    {
+        flag: '--help',
+        message: `Доступные команды:
+        --help    — печатает этот текст;
+        --version — печатает версию приложения;`
+    },
+    {
+        flag: '--version',
+        message: 'v0.0.1'
+    }
 ];
-const argv = process.argv;
 
-process.on('exit', (code) => {
-    if (code === 1) {
-        console.error('Неизвестная команда {{ имя команды }}.');
-        console.error('Чтобы прочитать правила использования приложения, наберите "--help"');
+const emnteredFlaggs = process.argv.filter((item, i) => {
+    if (i > 1) {
+        return item;
     }
 });
 
-if (argv.includes('--version')) {
-    console.log('v0.0.1');
-} else if(argv.includes('--help')) {
-    console.log('Доступные команды:');
-    console.log('--help    — печатает этот текст;');
-    console.log('--version — печатает версию приложения;');
+if (!emnteredFlaggs.length) {
+    return console.log('Kekstabooking. Design by Oleg Koltun.');
+}
 
-    validCommands.forEach((command) => {
-        console.log(command);
+function printMessage(com) {
+    console.log(com.message);
+}
+
+function printNotValidMessage(com) {
+    const notValidMessage = `Неизвестная команда ${com.flag}.
+ Чтобы прочитать правила использования приложения, наберите "--help"`;
+
+    console.log("\x1b[31m", notValidMessage, "\x1b[31");
+}
+
+emnteredFlaggs.forEach((flag) => {
+    const coms = validCoomands.filter((com) => {
+        if(com.flag === flag) {
+            printMessage(com);
+            return com;
+        }
     });
-} else {
-    process.exit(1);
-};
+
+    if (!coms.length) {
+        printNotValidMessage({ flag })
+        process.exit(1);
+    }
+});
