7bc2696958f346c5391606369df7d3b4344eb8a8
okoltun <okoltun@shift4.com>
2018-10-16 17:03:53 +0300

    add eslint

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const appVersion = 'v0.0.1';
+'use strict';
+
+const appVersion = `v0.0.1`;
 
 const validCommands = {
   help: {
-    flag: '--help',
-    action: function() {
+    flag: `--help`,
+    action() {
       console.log(this.message);
     },
     description: `печатает этот текст`
   },
   version: {
-    flag: '--version',
+    flag: `--version`,
     action() {
       console.log(appVersion);
     },
     description: `печатает версию приложения`
   },
   projectName: {
-    flag: '--app-name',
-    action: function() {
-      console.log('Супер приложение Кексобукинг');
+    flag: `--app-name`,
+    action() {
+      console.log(`Супер приложение Кексобукинг`);
     },
     description: `печатает название приложения`
   },
   author: {
-    flag: '--app-author',
-    action: function() {
-      console.log('Oleg Koltun');
+    flag: `--app-author`,
+    action() {
+      console.log(`Oleg Koltun`);
     },
     description: `печатает имя автора`
   }
@@ -35,40 +37,44 @@ const enteredFlags = process.argv.filter((item, i) => {
   if (i > 1) {
     return item;
   }
+  return false;
 });
 
-function createHelpMessage(validCommands) {
-  return Object.keys(validCommands).reduce((accum, com) => {
+function createHelpMessage(commands) {
+  return Object.keys(commands).reduce((accum, com) => {
     return `${accum}
-${validCommands[com].flag} - ${validCommands[com].description};`;
+${commands[com].flag} - ${commands[com].description};`;
   }, ``);
 }
 
 validCommands.help.message = createHelpMessage(validCommands);
 
 if (!enteredFlags.length) {
-  return console.log('Kekstabooking. Design by Oleg Koltun.');
+  console.log(`Kekstabooking. Design by Oleg Koltun.`);
 } else if (enteredFlags.length > 1) {
-  return console.log('\x1b[31m', 'Команда не может содержать более одного флага.');
+  console.log(`\x1b[31m`, `Команда не может содержать более одного флага.`);
 }
 
 function printNotValidMessage(com) {
   const notValidMessage = `Неизвестная команда ${com.flag}.
- Чтобы прочитать правила использования приложения, наберите '--help'`;
+    Чтобы прочитать правила использования приложения, наберите --help`;
 
-  console.log('\x1b[31m', notValidMessage);
+  return console.log(`\x1b[31m`, notValidMessage);
 }
 
 enteredFlags.forEach((flag) => {
-  const coms = Object.keys(validCommands).filter((com) => {
+  const commands = Object.keys(validCommands).filter((com) => {
     if (validCommands[com].flag === flag) {
       validCommands[com].action();
       return com;
     }
+    return false;
   });
 
-  if (!coms.length) {
-    printNotValidMessage({ flag });
-    process.exit(1);
+  if (!commands.length) {
+    printNotValidMessage({flag});
+    return process.exit(1);
   }
+
+  return false;
 });
